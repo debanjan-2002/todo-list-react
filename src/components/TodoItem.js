f0655999d0665eb5501b0ea11fb461e6f30bdc94
Debanjan Poddar <debanjan2602@gmail.com>
2022-12-28 17:15:05 +0530

    Added delete todo button and checkbox for marking todo as done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./TodoItem.module.css";
 
 const TodoItem = props => {
-    return <li className={styles.todo_item}>{props.todoText}</li>;
+    const [isDone, setIsDone] = useState(false);
+
+    const deleteTodoHandler = () => {
+        props.onDelete(props.id);
+    };
+
+    const strokeTodoHandler = () => {
+        console.log("Stroked");
+        setIsDone(!isDone);
+    };
+
+    return (
+        <li className={`${styles.todo_item} ${isDone && styles.done}`}>
+            <div className={styles.todo_item__stroke}>
+                <input type="checkbox" onChange={strokeTodoHandler} />
+            </div>
+            <div className={styles.todo_item__content}>{props.todoText}</div>
+            <div className={styles.todo_item__delete}>
+                <button onClick={deleteTodoHandler}>Delete</button>
+            </div>
+        </li>
+    );
 };
 
 export default TodoItem;
