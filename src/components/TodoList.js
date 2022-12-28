cb89a55efd76985cdc9332ca77d093129506b861
Debanjan Poddar <debanjan2602@gmail.com>
2022-12-28 17:17:03 +0530

    Lifted up the id of the "to be deleted" todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,10 +4,18 @@ import styles from "./TodoList.module.css";
 import TodoItem from "./TodoItem";
 
 const TodoList = props => {
+    const deleteTodoItemHandler = todoId => {
+        props.onDeleteTodo(todoId);
+    };
     return (
         <ul className={styles.todo_list__container}>
             {props.todos.map(todo => (
-                <TodoItem key={todo.id} todoText={todo.text} />
+                <TodoItem
+                    key={todo.id}
+                    todoText={todo.text}
+                    id={todo.id}
+                    onDelete={deleteTodoItemHandler}
+                />
             ))}
         </ul>
     );
